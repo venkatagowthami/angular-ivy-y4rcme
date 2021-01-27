a4c20f3b3c5c39260fb58ac43a05fc6be47507bb
venkatagowthami <gowthamivenkata6677@gmail.com>
2021-01-27 09:28:26 +0530

    2 files added

---
+++ src/app/animation/animation.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "my-animation",
+  templateUrl: "./animation.component.html"
+  // styleUrls: ["./animation.component.css"]
+})
+export class AnimationComponent {}
