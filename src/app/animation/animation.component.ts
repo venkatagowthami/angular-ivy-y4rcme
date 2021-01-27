4653d7f42b18c6e6169cae681bf21f6046c00ae6
venkatagowthami <gowthamivenkata6677@gmail.com>
2021-01-27 10:57:22 +0530

    1 file modified, 2 deleted

--- src/app/animation/animation.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from "@angular/core";
-
-@Component({
-  selector: "my-animation",
-  templateUrl: "./animation.component.html"
-  // styleUrls: ["./animation.component.css"]
-})
-export class AnimationComponent {}
